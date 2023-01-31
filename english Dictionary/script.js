5f17d54a48678ccb401113c72be29cff0d63d5f6
priya <priyankaraghuwanshi583@gmail.com>
2023-01-31 13:33:45 +0530

    new commit

---
+++ english Dictionary/script.js
@@ -0,0 +1,38 @@
+const input = document.getElementById('input');
+const title = document.getElementById('title');
+
+async function fetchApi(word){
+    if(word == ''){
+        p.innerHTML = 'please Enter any word'
+    }else{ try {
+      
+        p.style.display = 'block'
+        c1.style.display = 'none'
+        p.innerHTML = `Searching for a "${word}" ` 
+    
+    const url =await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/ ${word}`).then((res)=>res.json()) 
+    console.log(url);
+    
+    p.style.display = 'none'
+    c1.style.display = 'block'
+    title.innerHTML = url[0].word
+    meaning.innerHTML = url[0].meanings[0].definitions[0].definition
+
+    } catch (error) {
+        console.log(err);
+    }}
+   
+  
+
+}
+
+input.addEventListener('keyup',(e)=>{
+   
+if(e.key=="Enter"){
+ console.log(e.target.value);
+
+ fetchApi(e.target.value)
+ 
+
+
+}})
